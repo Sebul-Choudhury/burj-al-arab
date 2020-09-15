bed4140034c94dfb4d2720529f48f77b124fc0d7
Nazrul Chowdhury <nazrul@Nazruls-MacBook.local>
2020-09-15 23:59:08 +0600

    added code

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCcZRQMD_-SZ2cqpf9GimjxSRe21DP8Gac",
+    authDomain: "burj-al-arab-8982b.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-8982b.firebaseio.com",
+    projectId: "burj-al-arab-8982b",
+    storageBucket: "burj-al-arab-8982b.appspot.com",
+    messagingSenderId: "327100073169",
+    appId: "1:327100073169:web:b9fc7406d73838a7d931f0"
+  };
+
+  export default firebaseConfig;
